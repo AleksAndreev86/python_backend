ebcff7d9b2b209a8825ac6ab4d5844808c11f7be
Aleksandr <mrak808@gmail.com>
2024-09-16 16:03:19 +0500

    Решена задача со страницей, часть функционала которого перекрыта футером

---
+++ lesson2_2_step6.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time, math
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    link = "https://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, '#answer')
+    browser.execute_script("return arguments[0].scrollIntoView(true);", input1)
+    input1.send_keys(y)
+    checkbox = browser.find_element(By.CSS_SELECTOR, '#robotCheckbox')
+    browser.execute_script("return arguments[0].scrollIntoView(true);", checkbox)
+    checkbox.click()
+    radiobutton2 = browser.find_element(By.CSS_SELECTOR, '#robotsRule')
+    browser.execute_script("return arguments[0].scrollIntoView(true);", radiobutton2)
+    radiobutton2.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "body > div > form > button")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(10)
+    browser.quit()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
